401e6a8281ce7f250103a43edbd8c66989ab786f
xlaperriere <x.laperriere@gmail.com>
2023-12-03 11:57:43 -0500

    added one test funciton in MainWindowTest --> illegal arguments

--- src/test/MainWindowTest.java
+++ src/test/MainWindowTest.java
@@ -5,6 +5,7 @@ import currencyConverter.MainWindow;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,4 +18,23 @@ public class MainWindowTest {
             assertEquals(value, 1.0);
         }
     }
+
+    @Test
+    public void testIllegalArgs(){
+        ArrayList<Currency> currencies = Currency.init();
+        Random random = new Random();
+        Currency currencyOne = currencies.get(random.nextInt(6));
+        Currency currencyTwo = currencies.get(random.nextInt(6));
+        assertThrows(IllegalArgumentException.class, () -> {
+            // Negative amount
+            MainWindow.convert(currencyOne.getName(), currencyTwo.getName(), currencies, -100.0);
+            // Out of bounds amount (from specifications [0, 1 000 000] )
+            MainWindow.convert(currencyOne.getName(), currencyTwo.getName(), currencies, 1000001.0);
+            // Non-existent currency
+            MainWindow.convert(currencyOne.getName(), "N'importe quoi", currencies, 100.0);
+        });
+    }
+
+
+
 }
