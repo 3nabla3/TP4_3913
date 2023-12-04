9d7f48e6d54e16039f455c8467883c13f3b4b45f
xlaperriere <x.laperriere@gmail.com>
2023-12-04 18:49:29 -0500

    added test method for checking values

--- src/test/MainWindowTest.java
+++ src/test/MainWindowTest.java
@@ -19,6 +19,7 @@ public class MainWindowTest {
         }
         return null;
     }
+
     /**
      * Test if converting a currency to the same currency always returns
      * the same amount chosen.
@@ -32,6 +33,27 @@ public class MainWindowTest {
             assertEquals(1.0, value);
         }
     }
+
+    /**
+     * Test that all conversions return a positive Double value.
+     * black box test
+     */
+    @Test
+    public void testValueValidity() {
+        ArrayList<String> validCurrencies =
+                new ArrayList<>(Arrays.asList("USD", "CAD", "GBP", "EUR", "CHF", "AUD"));
+        ArrayList<Currency> currencies = Currency.init();
+
+        for (String curr1 : validCurrencies) {
+            String longName1 = convertShortNameToLongName(curr1, currencies);
+            for (String curr2 : validCurrencies) {
+                String longName2 = convertShortNameToLongName(curr2, currencies);
+                Double value = MainWindow.convert(longName1, longName2, currencies, 100.0);
+                assertTrue(value > 0d);
+            }
+        }
+    }
+
 
     /**
      * Test that all currencies in the specifications are implemented.
